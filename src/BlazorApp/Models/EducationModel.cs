77abd86ca136d161cade997a95aa216e1befa53c
DerSalik <mahamadarbil44@gmail.com>
2023-04-30 14:20:17 +0300

    education and experience

---
+++ src/BlazorApp/Models/EducationModel.cs
@@ -0,0 +1,9 @@
+namespace BlazorApp.Models;
+
+    public class EducationModel
+    {
+        public string? Degree { get; set; } = string.Empty;
+        public string? University { get; set; } = string.Empty;
+        public string? Year { get; set; } = string.Empty;
+    }
+
---
+++ src/BlazorApp/Models/ExperienceModel.cs
@@ -0,0 +1,13 @@
+namespace BlazorApp.Models
+{
+    public class ExperienceModel
+    {
+        public string? Position { get; set; }
+        public string? Company { get; set; }
+        public DateTime? StartDate { get; set; }
+        public DateTime? EndDate { get; set; }
+
+        public string? Location { get; set; }
+        public string? Description { get; set; }
+    }
+}
