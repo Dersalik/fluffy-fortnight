469e688c8a165582183b01cf75bddde96e158630
DerSalik <mahamadarbil44@gmail.com>
2024-12-31 18:09:13 +0300

    changes and new stuff added

--- src/BlazorApp/Models/BadgeInfo.cs
+++ src/BlazorApp/Models/BadgeInfo.cs
@@ -2,11 +2,11 @@ namespace BlazorApp.Models
 {
     public class BadgeInfo
     {
-        public string name { get; set; }
-        public string issuer { get; set; }
-        public string date { get; set; }
-        public string description { get; set; }
-        public string image { get; set; }
+        public string? name { get; set; }
+        public string? issuer { get; set; }
+        public string? date { get; set; }
+        public string? description { get; set; }
+        public string? image { get; set; }
         public string Url { get; set; } = string.Empty;
 
     }
--- src/BlazorApp/Models/SiteProperties.cs
+++ src/BlazorApp/Models/SiteProperties.cs
@@ -2,16 +2,10 @@ namespace BlazorApp.Models;
 
 public class SiteProperties
 {
-    public string Name { get; set; } = string.Empty;
-    public string Title { get; set; } = string.Empty;
-    public string Email { get; set; } = string.Empty;
-    public string DevDotTo { get; set; } = string.Empty;
-    public string GitHub { get; set; } = string.Empty;
-    public string Instagram { get; set; } = string.Empty;
-    public string LinkedIn { get; set; } = string.Empty;
-    public string Medium { get; set; } = string.Empty;
-    public string Twitter { get; set; } = string.Empty;
-    public string YouTube { get; set; } = string.Empty;
-    public string Pluralsight { get; set; } = string.Empty;
-
+    public String? Name { get; set; } = string.Empty;
+    public String? Title { get; set; } = string.Empty;
+    public String? Email { get; set; } = string.Empty;
+    public String? GitHub { get; set; } = string.Empty;
+    public String? LinkedIn { get; set; } = string.Empty;
+    public String? Pluralsight { get; set; } = string.Empty;
 }
